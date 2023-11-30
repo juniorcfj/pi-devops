ca84d4dfc28cb4575863984d964c53fa444d6182
juniorcfj <91996555+juniorcfj@users.noreply.github.com>
2023-11-30 09:35:39 -0300

    Update index.php

--- index.php
+++ index.php
@@ -52,13 +52,14 @@
 
 
 <script>
-    function abrirModal(nome, especie, sexo, idade, porte) {
+    function abrirModal(nome, especie, sexo, idade, porte, raca) {
         var modalContent = document.getElementById('modalContent');
         modalContent.innerHTML = '<p>Nome: ' + nome + '</p>' +
                                  '<p>Espécie: ' + especie + '</p>' +
                                  '<p>Sexo: ' + sexo + '</p>' +
                                  '<p>Idade: ' + idade + '</p>' +
-                                 '<p>Porte: ' + porte + '</p>';
+                                 '<p>Porte: ' + porte + '</p>' +
+                                 '<p>Raça: ' + raca + '</p>';
 
         var modal = document.getElementById('myModal');
         modal.style.display = 'block';
